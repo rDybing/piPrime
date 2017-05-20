282b7f30a351d22f6d3f35403e296918473eff77
rDybing <roy.dybing@gmail.com>
2017-05-20 17:47:18 +0200

    Working as intended thanks to /u/siritinga

--- piPrime.go
+++ piPrime.go
@@ -8,21 +8,15 @@ import (
 
 type output_t struct {
 	number uint64
-	time   float32
+	time   time.Duration
 	prime  int32
-}
-
-type timer_t struct {
-	timeOld int64
-	timeNew int64
 }
 
 func main() {
 	// manually setting CPU cores do nothing after go1.5
 	// runtime.GOMAXPROCS(4)
 	var countTo uint64
-	// set to 1.6 million - not 16 million - to save some time when debugging
-	countTo = 1600000
+	countTo = 16000000
 	fmt.Println("here we go. Ol'skool serial prime-finder")
 	primeSequential(countTo)
 	fmt.Println("and now the concurrent way!")
@@ -31,88 +25,59 @@ func main() {
 
 func primeSequential(limit uint64) {
 	var out output_t
-	var timer timer_t
-	timer.timeOld = millis()
+	now := time.Now()
 
 	for out.number = 2; out.number < limit; out.number++ {
 		if isPrime(out.number) {
 			out.prime++
 		}
 		if (out.number)%1000000 == 0 {
-			out.time = getTimePassed(&timer)
+			out.time = time.Since(now)
 			output(&out, 0)
 		}
 	}
-	out.time = getTimePassed(&timer)
+	out.time = time.Since(now)
 	output(&out, 0)
 }
 
 func primeConcurrent(limit uint64) {
 	var out output_t
-	var timer timer_t
-	var isTrue bool
 
 	chRec := make(chan bool, 100)
 	chSend := make(chan uint64, 100)
 
-	timer.timeOld = millis()
+	now := time.Now()
 
 	// spin up four goroutines - one for each CPU core
 	for i := 0; i < 4; i++ {
 		go isPrimeConc(chSend, chRec)
 	}
-
-	for out.number = 2; out.number < limit; out.number += 4 {
-		// send
-		for i := 0; i < 4; i++ {
-			chSend <- out.number + uint64(i)
+	go func(limit uint64, chSend chan uint64) {
+		for i := uint64(2); i < limit; i++ {
+			// send
+			chSend <- i
 		}
-		// receive
-		for i := 0; i < 4; i++ {
-			isTrue = <-chRec
-			if isTrue {
-				out.prime++
-			}
-			if (out.number+uint64(i))%1000000 == 0 {
-				out.time = getTimePassed(&timer)
-				output(&out, i)
-			}
+	}(limit, chSend)
+	// receive
+	for i := uint64(2); i < limit; i++ {
+		if <-chRec {
+			out.prime++
+		}
+		if i%1000000 == 0 {
+			out.time = time.Since(now)
+			output(&out, i)
 		}
 	}
-	out.time = getTimePassed(&timer)
+	out.number = limit
+	out.time = time.Since(now)
 	output(&out, 0)
+	close(chRec)
+	close(chSend)
 }
 
 func isPrimeConc(inC <-chan uint64, out chan<- bool) {
-	var i uint64
-	var s uint64
-	var in uint64
-	var flag bool
-	var exit bool
-
-	for {
-		exit = false
-		in = <-inC
-
-		s = uint64(math.Sqrt(float64(in)))
-
-		if in > 2 && in%2 == 0 {
-			flag = false
-			exit = true
-		}
-		if exit == false {
-			for i = 3; i <= s; i += 2 {
-				if in%i == 0 {
-					flag = false
-					exit = true
-					break
-				}
-			}
-		}
-		if exit == false {
-			flag = true
-		}
-		out <- flag
+	for i := range inC {
+		out <- isPrime(i)
 	}
 }
 
@@ -131,19 +96,6 @@ func isPrime(number uint64) bool {
 	return true
 }
 
-func output(o *output_t, i int) {
-	fmt.Printf("%8d || time: %6.2f || primes: %d\n", o.number+uint64(i), o.time, o.prime)
-}
-
-func getTimePassed(t *timer_t) float32 {
-	var timeSec float32
-	var timeTemp int64
-	t.timeNew = millis()
-	timeTemp = t.timeNew - t.timeOld
-	timeSec = float32(timeTemp) / float32(1000)
-	return timeSec
-}
-
-func millis() int64 {
-	return time.Now().Round(time.Millisecond).UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
+func output(o *output_t, i uint64) {
+	fmt.Printf("%8d || time: %v || primes: %d\n", o.number+i, o.time, o.prime)
 }
